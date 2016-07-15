63ce2d2648c89850b41315b6c7ba1ccd20382f6a
Rainer <it_jinlei.yue@163.com>
2016-07-15 11:22:48 +0800

    Initial Commit

---
+++ 20160715001-Thread-GCD-Singalton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160715001-Thread-GCD-Singalton
+//
+//  Created by Rainer on 16/7/15.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
